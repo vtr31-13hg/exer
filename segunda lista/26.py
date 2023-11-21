9afd90d9bb9a10f7cc87596ef281db2bedffe510
vtr31-13hg <144157603+vtr31-13hg@users.noreply.github.com>
2023-11-21 14:11:51 -0300

    Update 26.py

--- segunda lista/26.py
+++ segunda lista/26.py
@@ -1,9 +1,9 @@
-pr1=float(input('digite o preço de um produto'))
-pr2=float(input('digite o preço do segundo produto'))
-pr3=float(input('digite o preço de outro produto'))
-if p1<p2 and p3:
+pr1=float(input('digite o preço de um produto: '))
+pr2=float(input('digite o preço do segundo produto: '))
+pr3=float(input('digite o preço de outro produto: '))
+if pr1<pr2 and pr3:
     print('compre esse produto de {}'.format(pr1))
-elif p2<p1 and p3:
+elif pr2<pr1 and pr3:
     print('compre esse produto de {}'.format(pr2))
 else:
     print('compre o produto que custa {}'.format(pr3))
